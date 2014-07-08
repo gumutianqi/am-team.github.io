3604348895c3bdd51a3a6dbc0484b4dbcca857bf
轩与 <wangwei.wang@alipay.com>
2014-07-08 15:29:52 +0800

    功能修正

--- static/global.js
+++ static/global.js
@@ -3,6 +3,8 @@ define(function(require, exports, module) {
     $('.entry-content a').attr('target','_blank');
     //左侧菜单定位
     $(function(){
+        //输入锚点自动定位
+        $("#sidebar-fixed-nav a[href=" + window.location.hash + "]").click();
         //获取要定位元素距离浏览器顶部的距离
         var navTop = $(".header-nav").offset().top;
         //获取要定位元素与边栏的相对距离
@@ -25,31 +27,28 @@ define(function(require, exports, module) {
                 $("#sidebar-wrapper").removeClass('sidebar-fixed-top').offset({top: nav_maxBottom + sidebarBtmFixed});
                 $(".navigation").addClass('navigation-fix navigation-btm').offset({top: nav_maxBottom});
             }
+            $("#sidebar-wrapper").css({maxHeight: $(window).height() - 87});
         })
+        $("#sidebar-wrapper").css({maxHeight: $(window).height() - 87});
     })
     
     // var autoc = require('arale/autocomplete/1.2.3/autocomplete');
     // console.log(autoc);
-    var contentNode = $('.document .entry-content');
-    var titleNodes=contentNode.find('h2,h3');
+    var titleNodes=$('.document .entry-content').find('h2,h3');
     var sidebar=$('.threecol.meta');
     var htmlStr='';
     htmlStr+='<div id="sidebar-wrapper">';
     htmlStr+='<div id="sidebar-fixed-nav">';
     titleNodes.each(function(index){
-        if($(this).attr('id')){
-            switch(this.nodeName.toUpperCase()){
-                case 'H2':
-                    if(index!==0){
-                        htmlStr+='</dl>';
-                    }
-                    htmlStr+='<dl>';
-                    htmlStr+=('<dt data-index="'+index+'"><a href="#'+$(this).attr('id')+'">' +$(this).text()+'</a></dt>');
-                    break;
-                case 'H3':
-                    htmlStr+=('<dd data-index="'+index+'"><a href="#'+$(this).attr('id')+'">' +$(this).text()+'</a></dd>');
-                    break;
+        if($(this).attr('id') && $(this).is("h2")){
+            if(index!==0){
+                htmlStr+='</dl>';
             }
+            htmlStr+='<dl>';
+            htmlStr+=('<dt data-index="'+index+'"><a href="#'+$(this).attr('id')+'">' +$(this).text()+'</a></dt>');
+        }
+        else if($(this).attr('id') && $(this).is("h3")){
+            htmlStr+=('<dd data-index="'+index+'"><a href="#'+$(this).attr('id')+'">' +$(this).text()+'</a></dd>');
         }
     });
     htmlStr+='</dl>';
@@ -62,7 +61,7 @@ define(function(require, exports, module) {
     });
     function titleActive(){
         titleNodes.each(function(index){
-            if(this.getBoundingClientRect().bottom>-40 && this.getBoundingClientRect().top<window.innerHeight){
+            if(this.getBoundingClientRect().bottom > 17 && this.getBoundingClientRect().top < window.innerHeight){
                 var curNode=sidebar.find('[data-index="'+index+'"]');
                 curNode.closest('dl').siblings('dl').removeClass('active');
                 curNode.closest('dl').addClass('active');
@@ -86,8 +85,10 @@ define(function(require, exports, module) {
         $("body").animate({scrollTop: '0px'}, 800);
     });
     $("#sidebar-fixed-nav a").click(function () {       //动画滚动到指定锚点
-        var t = $(this);
+        history.pushState && event.preventDefault();    //向下兼容
+        var t = $(this), url;
         $("body").animate({scrollTop: $(t.attr("href")).offset().top - 57}, 800);
-        return false;
-    })
+        url = window.location.pathname.split("/");
+        history.pushState({}, "", url[url.length - 1] + t.attr("href"));
+    });
 });
